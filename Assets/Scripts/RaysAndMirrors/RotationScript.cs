0c32b9b6e6d749d87b3e555e8851fa2109dfac45
latinrice <92548405+latinrice@users.noreply.github.com>
2022-12-03 16:59:32 +0300

    Add sprites to CornerMirror (without animations)

--- Assets/Scripts/RaysAndMirrors/RotationScript.cs
+++ Assets/Scripts/RaysAndMirrors/RotationScript.cs
@@ -7,6 +7,15 @@ public class RotationScript : MonoBehaviour
     public bool playerIsNear = false;
     [SerializeField]
     private GameObject block;
+    [SerializeField]
+    private Sprite[] sprites = new Sprite[4];
+    private SpriteRenderer spriteRenderer;
+    private int rotation = 0;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponentInParent<SpriteRenderer>();
+    }
 
     private void Update()
     {
@@ -14,7 +23,7 @@ public class RotationScript : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
-                block.transform.Rotate(0, 0, 90);
+                RotateBlock();
             }
         }
     }
@@ -29,5 +38,20 @@ public class RotationScript : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
             playerIsNear = false;
+    }
+
+    private void RotateBlock()
+    {
+        block.transform.Rotate(0, 0, -90);
+        rotation = (rotation + 1) % 4;
+        if (rotation == 2)
+        {
+            spriteRenderer.sortingOrder += 2;
+        }
+        else if (rotation == 0)
+        {
+            spriteRenderer.sortingOrder -= 2;
+        }
+        spriteRenderer.sprite = sprites[rotation];
     }
 }
