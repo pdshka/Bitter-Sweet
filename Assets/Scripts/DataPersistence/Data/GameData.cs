7201a0022d9af48aa887414d15b004b8726978d9
latinrice <92548405+latinrice@users.noreply.github.com>
2023-03-31 00:41:22 +0300

    Add scene for monkey coconut game

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -25,10 +25,10 @@ public class GameData
         teleporters.Add("97b5e0da-34b0-4bcf-b775-db3aab49e260", new Vector3(0f, 2.5f, 0f)); // Cafe-Jungle
         teleporters.Add("00656c61-486a-4899-872b-03a5524b67c0", new Vector3(-6.5f, 5.5f, 0f)); // Jungle-Cafe
         teleporters.Add("08635aab-e008-46ef-8f9c-8762ee8030dd", new Vector3(1.5f, -6.5f, 0f)); // Jungle-Maze
-        teleporters.Add("2ecc4656-b196-420e-87ce-910543c7cafc", new Vector3(0.5f, 9f, 0f)); // Jungle-Temple
-        teleporters.Add("5da5dfb1-abb4-45e6-9b73-7035e50bc49d", new Vector3(-0.5f, -8.5f, 0f)); // Temple-Jungle
-        teleporters.Add("ea251a03-822a-4c40-92dd-3cea989fe827", new Vector3(-0.5f, 7.5f, 0f)); // Temple-Pipes
-        teleporters.Add("edcf78b8-8005-4c43-bb60-5a78cc5fe010", new Vector3(-11.5f, 4.5f, 0f)); // Pipes-Temple
+        teleporters.Add("2ecc4656-b196-420e-87ce-910543c7cafc", new Vector3(0.5f, 9f, 0f)); // Jungle-WaterTemple
+        teleporters.Add("5da5dfb1-abb4-45e6-9b73-7035e50bc49d", new Vector3(-0.5f, -8.5f, 0f)); // WaterTemple-Jungle
+        teleporters.Add("ea251a03-822a-4c40-92dd-3cea989fe827", new Vector3(-0.5f, 7.5f, 0f)); // WaterTemple-Pipes
+        teleporters.Add("edcf78b8-8005-4c43-bb60-5a78cc5fe010", new Vector3(-11.5f, 4.5f, 0f)); // Pipes-WaterTemple
         teleporters.Add("cb4f543d-a40e-4983-be7f-96acb46e4b6d", new Vector3(-0.5f, 2.5f, 0f)); // Maze-Jungle
         teleporters.Add("a48c36e9-f8c3-4ae7-a3c8-c465333108f2", new Vector3(-20.5f, -38f, 0f)); // Maze-Rays6
         teleporters.Add("3e8c9c2a-5992-43e2-8387-5a4b0fcb0eac", new Vector3(-6f, 5f, 0f)); // Rays6-Maze
@@ -42,5 +42,8 @@ public class GameData
         teleporters.Add("99c91028-5890-4612-98cb-b323b3f673ed", new Vector3(0.5f, 2.5f, 0)); // Rays3-Maze
         teleporters.Add("15068248-d126-4cb2-be68-d2c3a6fc60f2", new Vector3(-2.5f, 3.5f, 0)); // Rays4-Maze
         teleporters.Add("28692330-cade-41a3-9adf-b6e41a047331", new Vector3(-3, 6, 0)); // Rays5-Maze
+        teleporters.Add("75970152-940d-461e-bc97-9d0d115ce9b1", new Vector3(-10.5f, 0.5f, 0)); // Jungle-MonkeyCoconuts
+        teleporters.Add("d0ffc720-58c3-4db8-9594-646087819af9", new Vector3(-0.5f, 0.5f, 0)); // MonkeyCoconuts-Jungle 
+        teleporters.Add("a63fe7d3-c232-45a1-affb-20dbaf88b7a9", new Vector3(6.5f, 0.5f, 0)); //MonkeyCoconuts-MonkeyTicTacToe
     }
 }
